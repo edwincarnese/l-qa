6020da45d8e57fab006e00f32533724b38bf8dac
Edwin Carnese <edwincarnese@gmail.com>
2021-02-09 14:46:12 +0800

    init

---
+++ resources/assets/js/mixins/modification.js
@@ -0,0 +1,88 @@
+import Vote from '../components/Vote.vue';
+import UserInfo from '../components/UserInfo.vue';
+import MEditor from '../components/MEditor.vue';
+import highlight from './highlight';
+
+export default {
+    mixins: [highlight],
+
+    data() {
+        return {
+            editing: false,
+        }
+    },
+
+    components: {
+        Vote,
+        UserInfo,
+        MEditor
+    },
+
+    methods: {
+        edit() {
+            this.setEditCache();
+            this.editing = true;
+        },
+
+        cancel() {
+            this.restoreFromCache();
+            this.editing = false;
+        },
+
+        setEditCache() {
+
+        },
+
+        restoreFromCache() {
+
+        },
+
+        update() {
+            axios.put(this.endpoint, this.payload())
+            .then(({data}) => {
+                this.bodyHtml = data.body_html;
+                this.$toast.success(data.message, "Success", { timeout: 3000 });
+                this.editing = false;
+            })
+            .catch(({response}) => {
+                this.$toast.error(response.data.message, "Error", { timeout: 3000});
+            })
+            .then(() => this.highlight());
+        },
+
+        payload() {
+
+        },
+
+        destroy () {
+            this.$toast.question('Are you sure about that?', "Confirm", {
+            timeout: 20000,
+            close: false,
+            overlay: true,
+            displayMode: 'once',
+            id: 'question',
+            zindex: 999,
+            title: 'Hey',            
+            position: 'center',
+            buttons: [
+                ['<button><b>YES</b></button>', (instance, toast) => {
+
+                    this.delete();
+
+                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+
+                }, true],
+                ['<button>NO</button>', function (instance, toast) {
+
+                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+
+                }],
+            ]            
+            });            
+        },
+
+        delete() {
+            
+        }
+    }
+}
